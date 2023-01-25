6255f0d8a113e6303528ecddd3ec6a48ae057714
Kameron Comer <kam.comer96@gmail.com>
2023-01-24 22:59:40 -0500

    Changed event handler to bot to implement commands

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import discord
+from discord.ext import commands
+
 import os
 from dotenv import load_dotenv
 from datetime import datetime
@@ -8,26 +10,36 @@ TOKEN = os.getenv('BOT_TOKEN')
 GUILD = os.getenv('DISCORD_SERVER')
 
 intents = discord.Intents().all()
-client = discord.Client(intents=intents)
+# client = discord.client(intents=intents)
+bot = commands.Bot(intents=intents, command_prefix='!')
 
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f'{client.user.name} has connect to Discord!')
+    print(f'{bot.user.name} has connect to Discord!')
 
 
-@client.event
-async def on_member_join(member):
-    await member.create_dm()
-    await member.dm_channel.send(
-            f'Hi {member.name}, welcome to my Discord Server!')
+# @client.event
+# async def on_ready():
+#     print(f'{client.user.name} has connect to Discord!')
+
+# @client.event
+# async def on_member_join(member):
+#     await member.create_dm()
+#     await member.dm_channel.send(
+#             f'Hi {member.name}, welcome to my Discord Server!')
 
 
-@client.event
-async def on_message(message):
-    if 'time stamp' in message.content.lower():
-        time_stamp = str(datetime.now())
-        print(time_stamp)
-        await message.channel.send(time_stamp)
+# @client.event
+# async def on_message(message):
+#     if 'time stamp' in message.content.lower():
+#         time_stamp = str(datetime.now())
+#         print(time_stamp)
+#         await message.channel.send(time_stamp)
 
-client.run(TOKEN)
+
+@bot.command(name='test', help="Does the thing")
+async def test(ctx):
+    await ctx.send('Hello World!')
+
+bot.run(TOKEN)
